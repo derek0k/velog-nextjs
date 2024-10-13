98c33e3133e0f0bd1b6aef095114243d0dfb0f0f
derek0k <derek0kim@gmail.com>
2024-10-14 00:17:26 +0900

    redirects & rewrites

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,28 @@ const nextConfig = {
   images: {
     remotePatterns: [{ protocol: "https", hostname: "images.unsplash.com" }],
   },
+  async redirects() {
+    return [
+      {
+        source: "/products/deleted_forever",
+        destination: "products",
+        permanent: true,
+      },
+      {
+        source: "/products/deleted_temp",
+        destination: "products",
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/items/:slug",
+        destination: "/products/:slug",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
