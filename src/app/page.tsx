b48942c3d2dc61f3a50cc7c10ce623341d5c0987
derek0k <derek0kim@gmail.com>
2024-10-15 03:03:25 +0900

    Hero.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,10 @@
+import Hero from "@/components/Hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <h1>Home</h1>
-      <Image
-        src="https://images.unsplash.com/photo-1441986300917-64674bd600d8"
-        alt="shop"
-        width={400}
-        height={400}
-      />
+      <Hero />
     </>
   );
 }
