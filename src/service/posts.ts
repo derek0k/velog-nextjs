1d5cce9ace3dab4cb9a74f08295de74bb3a0248c
derek0k <derek0kim@gmail.com>
2024-10-16 01:45:15 +0900

    add: AdjacentPostCard

--- src/service/posts.ts
+++ src/service/posts.ts
@@ -10,7 +10,11 @@ export type Post = {
   featured: boolean;
 };
 
-export type PostData = Post & { content: string };
+export type PostData = Post & {
+  content: string;
+  next: Post | null;
+  prev: Post | null;
+};
 
 export async function getFeaturedPosts(): Promise<Post[]> {
   return getAllPosts().then((posts) => posts.filter((post) => post.featured));
@@ -25,16 +29,21 @@ export async function getAllPosts(): Promise<Post[]> {
   const data = await fs.readFile(filePath, "utf-8");
   const posts: Post[] = JSON.parse(data);
 
+  // 게시물을 최신순으로 정렬함
   return posts.sort((a, b) => (a.date > b.date ? -1 : 1));
 }
 
 export async function getPostData(fileName: string): Promise<PostData> {
   const filePath = path.join(process.cwd(), "data", "posts", `${fileName}.md`);
+  const posts = await getAllPosts();
+  const post = posts.find((post) => post.path === fileName);
+
+  if (!post) throw new Error(`Doesn't exist file name : ${fileName} `);
+
+  const index = posts.indexOf(post);
+  // 최신 순으로 정렬되어 있으므로, 다음 페이지는 인덱스 -1에 해당한다
+  const next = index > 0 ? posts[index - 1] : null;
+  const prev = index < posts.length - 1 ? posts[index + 1] : null;
   const content = await fs.readFile(filePath, "utf-8");
-
-  const metadata = await getAllPosts() //
-    .then((posts) => posts.find((post) => post.path === fileName));
-  if (!metadata) throw new Error(`Doesn't exist file name : ${fileName} `);
-
-  return { ...metadata, content };
+  return { ...post, content, next, prev };
 }
