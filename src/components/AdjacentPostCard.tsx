5064b5d2799cb52b6cb96cf53bbefeef7c95a107
derek0k <derek0kim@gmail.com>
2024-10-16 09:10:20 +0900

    style: responsive design - PostPage

--- src/components/AdjacentPostCard.tsx
+++ src/components/AdjacentPostCard.tsx
@@ -15,7 +15,10 @@ export default function AdjacentPostCard({ post, type }: Props) {
   const { path, title, description } = post;
 
   return (
-    <Link href={`/posts/${path}`} className="relative w-full bg-black max-h-56">
+    <Link
+      href={`/posts/${path}`}
+      className="relative w-full bg-black max-h-28 sm:max-h-56"
+    >
       <Image
         className="w-full opacity-40"
         src={`/images/posts/${path}.png`}
@@ -26,8 +29,8 @@ export default function AdjacentPostCard({ post, type }: Props) {
       <div className="group absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full flex justify-around items-center px-8">
         {type === "prev" && <FaArrowLeft className={ICON_CLASS} />}
         <div className="w-full text-center">
-          <h3 className="text-3xl font-bold">{title}</h3>
-          <p className="font-bold">{description}</p>
+          <h3 className="text-xl sm:text-3xl font-bold">{title}</h3>
+          <p className="hidden sm:block font-bold">{description}</p>
         </div>
         {type === "next" && <FaArrowRight className={ICON_CLASS} />}
       </div>
