f24ea707ee058d36c9392701d48533e37e0ee569
derek0k <derek0kim@gmail.com>
2024-10-16 21:10:34 +0900

    fix: optimize data fetching with caching

--- src/service/posts.ts
+++ src/service/posts.ts
@@ -1,5 +1,6 @@
 import path from "path";
 import { promises as fs } from "fs";
+import { cache } from "react";
 
 export type Post = {
   title: string;
@@ -24,14 +25,16 @@ export async function getNonFeaturedPosts(): Promise<Post[]> {
   return getAllPosts().then((posts) => posts.filter((post) => !post.featured));
 }
 
-export async function getAllPosts(): Promise<Post[]> {
+export const getAllPosts = cache(async (): Promise<Post[]> => {
   const filePath = path.join(process.cwd(), "data", "posts.json");
   const data = await fs.readFile(filePath, "utf-8");
   const posts: Post[] = JSON.parse(data);
 
+  console.log("getAllPosts called");
+
   // 게시물을 최신순으로 정렬함
   return posts.sort((a, b) => (a.date > b.date ? -1 : 1));
-}
+});
 
 export async function getPostData(fileName: string): Promise<PostData> {
   const filePath = path.join(process.cwd(), "data", "posts", `${fileName}.md`);
